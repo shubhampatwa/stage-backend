9176ec6b9d8601c9d07b8ddce1d43a4b41f670be
shubhampatwa <shubhampatwa526@gmail.com>
2024-12-16 09:45:14 +0530

    feat: added seeding on main module init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,27 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MoviesModule } from './movies/movies.module';
 import { TvshowsModule } from './tvshows/tvshows.module';
+import { SeedService } from './seed/seed.service';
+import { User, UserSchema } from './models/user.schema';
+import { TVShow, TVShowSchema } from './models/tvshow.schema';
+import { Movie, MovieSchema } from './models/movie.schema';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://mongodb:27017/stagedb'),
+    MongooseModule.forRoot('mongodb://localhost:27017/stagedb'),
     MoviesModule,
     TvshowsModule,
+    MongooseModule.forFeature([
+      { name: TVShow.name, schema: TVShowSchema },
+      { name: Movie.name, schema: MovieSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
   ],
+  providers: [SeedService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly seedService: SeedService) {}
+  async onModuleInit() {
+    await this.seedService.seedDatabase();
+  }
+}
